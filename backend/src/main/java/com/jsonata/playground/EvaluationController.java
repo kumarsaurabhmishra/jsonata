d61283feb573c79be6294a9502795ba0ad908665
Kumar Saurabh Mishra <abcsaurabhxyz3@gmail.com>
2026-02-04 14:46:41 +0530

    Fix: Remove unused imports causing build failure

--- backend/src/main/java/com/jsonata/playground/EvaluationController.java
+++ backend/src/main/java/com/jsonata/playground/EvaluationController.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.api.jsonata4java.Expression;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
